9e0189197093f2c8e79fcde40624f78a60af30d0
Christian Brown <54856120+christianjbrown@users.noreply.github.com>
2023-11-20 00:13:15 +0000

    Add capabilities key constant to transformer interface

--- src/Transformer/DeviceComponentCapabilitiesTransformerInterface.php
+++ src/Transformer/DeviceComponentCapabilitiesTransformerInterface.php
@@ -6,5 +6,7 @@ namespace ChristianBrown\SmartThings\Transformer;
 
 interface DeviceComponentCapabilitiesTransformerInterface
 {
+    public const ID_VALUE_TEMPERATURE_MEASUREMENT = 'temperatureMeasurement';
+
     public function transform(array $data): array;
 }
