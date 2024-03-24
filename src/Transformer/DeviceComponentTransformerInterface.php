5346efa8594cbc1a7c77670028aabcf7519798d9
Christian Brown <54856120+christianjbrown@users.noreply.github.com>
2024-03-24 11:50:51 +0000

    Add tests for DeviceComponentTransformer

--- src/Transformer/DeviceComponentTransformerInterface.php
+++ src/Transformer/DeviceComponentTransformerInterface.php
@@ -9,6 +9,7 @@ use ChristianBrown\SmartThings\Model\DeviceComponentInterface;
 interface DeviceComponentTransformerInterface
 {
     public const KEY_CAPABILITIES = 'capabilities';
+    public const UNEXPECTED_ARRAY_SPRINTF = '%s not set or not an array';
 
     public function transform(array $data): DeviceComponentInterface;
 }
