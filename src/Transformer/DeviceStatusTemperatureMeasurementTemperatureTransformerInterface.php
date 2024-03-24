b5ea6b68a03fe478324b67e30ef4fd1eeaf5af7a
Christian Brown <54856120+christianjbrown@users.noreply.github.com>
2024-03-24 12:15:55 +0000

    Add tests for temperature-measurement leaf transformer

--- src/Transformer/DeviceStatusTemperatureMeasurementTemperatureTransformerInterface.php
+++ src/Transformer/DeviceStatusTemperatureMeasurementTemperatureTransformerInterface.php
@@ -11,6 +11,10 @@ interface DeviceStatusTemperatureMeasurementTemperatureTransformerInterface
     public const KEY_TIMESTAMP = 'timestamp';
     public const KEY_UNIT = 'unit';
     public const KEY_VALUE = 'value';
+    public const UNEXPECTED_ARRAY_SPRINTF = '%s not set or not an array';
+    public const UNEXPECTED_DATA_TIMESTAMP = '%s not a valid timestamp';
+    public const UNEXPECTED_FLOAT_SPRINTF = '%s not set or not a floating point number';
+    public const UNEXPECTED_STRING_SPRINTF = '%s not set or not a string';
 
     public function transform(array $data): DeviceStatusTemperatureMeasurementTemperatureInterface;
 }
