ffbdc7d68fdde322b1a529f0d0ba8b6bcabac35b
Christian Brown <54856120+christianjbrown@users.noreply.github.com>
2024-03-24 11:20:50 +0000

    Cover optional device fields in DeviceTransformer

--- src/Transformer/DeviceTransformerInterface.php
+++ src/Transformer/DeviceTransformerInterface.php
@@ -12,6 +12,7 @@ interface DeviceTransformerInterface
     public const KEY_DEVICE_ID = 'deviceId';
     public const KEY_LABEL = 'label';
     public const KEY_NAME = 'name';
+    public const UNEXPECTED_STRING_SPRINTF = '%s not set or not a string';
 
     public function transform(array $data): DeviceInterface;
 }
